d244c843c1c3795fdee8d83dc0f640f784d91f92
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2020-04-16 15:45:09 +0700

    handle login google

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC-uDCNNsAflp86EXuhfpBnljoJiihxx6o",
+    authDomain: "fir-react-36fd1.firebaseapp.com",
+    databaseURL: "https://fir-react-36fd1.firebaseio.com",
+    projectId: "fir-react-36fd1",
+    storageBucket: "fir-react-36fd1.appspot.com",
+    messagingSenderId: "303554419068",
+    appId: "1:303554419068:web:a8f318f0025fe8aeea2cf9"
+  };
